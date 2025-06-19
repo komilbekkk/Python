d45decfc66570fb63770b4e5e3a55860d5218360
Komilbek Abdugafforov <komilbekabdugafforov@gmail.com>
2025-06-19 20:57:22 +0500

    Update 1.py

--- lesson-1/homework/1.py
+++ lesson-1/homework/1.py
@@ -1,1 +1,44 @@
+# Homework:
+# 1
 
+side = float(input('Enter the lenght of the square'))
+
+perimetr = side*4
+area = side**2
+
+print('Perimetr of the square:', perimetr)
+print('Area of the square:', area)
+
+# 2
+
+pi = 3.14159
+
+diameter = float(input("Enter the diameter of the circle: "))
+
+circumference = pi * diameter
+
+print("Circumference of the circle:", circumference)
+
+# 3
+
+a = float(input("Enter the first number (a): "))
+b = float(input("Enter the second number (b): "))
+
+mean = (a + b) / 2
+
+print("The mean is:", mean)
+
+# 4
+
+a = float(input("Enter the first number (a): "))
+b = float(input("Enter the second number (b): "))
+
+sum = (a + b)
+product = (a*b)
+square_a = (a**2)
+square_b = (b**2)
+
+print("The sum is:", sum)
+print("The product is:", product)
+print("The square_a is:", square_a)
+print("The square_b is:", square_b)
