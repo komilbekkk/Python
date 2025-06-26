27aaa96fd37c73c06ec52ff680dfefc154d67da2
Komilbek Abdugafforov <komilbekabdugafforov@gmail.com>
2025-06-26 09:43:03 +0500

    Create lesson3.py

---
+++ lesson-3/homework/lesson3.py
@@ -0,0 +1,40 @@
+# Homework: List and Tuple Exercises
+
+# 1
+
+fruits = ["apple", "banana", "cherry", "orange", "grape"]
+print(fruits[2]) 
+
+# 2
+
+l1 = [1,2,3,4,5]
+l2 = [6,7,8,9,10]
+l3 = l1 +l2
+
+print("Combined list:", l3)
+
+# 3
+
+l4 = [1,2,3,4,5,6,7,8]
+
+first_n = l4[0]
+
+middle_n = l4[len(l4)//2]
+
+last_n = l4[-1]
+
+new_list = [first_n, middle_n, last_n]
+
+print(new_list)
+
+# 4
+
+movie = ['The Godfather', 'Stive Jobs', 'Inside', 'Work', 'Jobless']
+
+converted = tuple(movie)
+
+print(converted)
+
+# 5
+
+
